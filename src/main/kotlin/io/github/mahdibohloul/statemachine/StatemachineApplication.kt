29ae7b47d7b0056916a4a09b0d4746d5a9caca4c
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-10-07 15:58:09 +0330

    Initialize Spring Boot project with Kotlin, Gradle, and essential configurations.

---
+++ src/main/kotlin/io/github/mahdibohloul/statemachine/StatemachineApplication.kt
@@ -0,0 +1,11 @@
+package io.github.mahdibohloul.statemachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatemachineApplication
+
+fun main(args: Array<String>) {
+  runApplication<StatemachineApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/mahdibohloul/statemachine/StatemachineApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mahdibohloul.statemachine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatemachineApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
